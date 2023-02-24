582d8bb9026af6c70b52e5ea3afd555b17870496
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2023-02-24 13:19:25 +0530

    Add json stringify and cors to communicate with the frontend

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const { urlencoded } = require("express");
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 const mongoose = require("mongoose");
 const projectsRouter = require("./routes/Projects");
@@ -9,6 +10,7 @@ const app = express();
 mongoose.set('strictQuery', false);
 mongoose.connect(process.env.MONGODBURL);
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/Projects", projectsRouter);
--- routes/Users.js
+++ routes/Users.js
@@ -7,7 +7,7 @@ userRouter.get("/:username", async (req, res) => {
     //Display a unique user
     const { username } = req.params;
     const user = await User.findOne({ username });
-    return res.send(user);
+    return res.send(JSON.stringify(user));
 });
 
 userRouter.post("/", async (req, res) => {
@@ -16,7 +16,7 @@ userRouter.post("/", async (req, res) => {
     const { username, firstName, lastName, password, email } = req.body;
     const hashedPassword = bcrypt.hashSync(password, 8);
     const user = await User.create({ username, firstName, lastName, password: hashedPassword, email });
-    return res.send(user);
+    res.send(JSON.stringify(user));
 });
 
 userRouter.delete("/:username", (req, res) => {
