508e2875ceaedce1f9cbd99ae4116c509fbe7aa4
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2023-02-24 15:56:01 +0530

    Created sessions for users

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
-const { urlencoded } = require("express");
 const express = require("express");
+const session = require('express-session')
 const cors = require("cors");
 require("dotenv").config();
 const mongoose = require("mongoose");
 const projectsRouter = require("./routes/Projects");
 const userRouter = require("./routes/Users");
+const AuthRouter = require("./routes/Authentication");
 const app = express();
 
 mongoose.set('strictQuery', false);
 mongoose.connect(process.env.MONGODBURL);
+
+app.use(session({
+    secret: 'this is a really long string',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}))
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/Projects", projectsRouter);
 app.use("/Users", userRouter);
+app.use("/Auth", AuthRouter);
 
 app.listen(process.env.PORT, () => console.log("Listening on port 3000"));
